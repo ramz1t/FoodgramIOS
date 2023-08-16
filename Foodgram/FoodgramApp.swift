c6b6077a9ac8eb365436717a7eebf4443de8306c
Timur Ramazanov <timurram007@gmail.com>
2023-08-17 01:14:34 +0200

    Initial Commit

---
+++ Foodgram/FoodgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodgramApp.swift
+//  Foodgram
+//
+//  Created by Timur Ramazanov on 17.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
